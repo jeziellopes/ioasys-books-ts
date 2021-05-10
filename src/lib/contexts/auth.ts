35e700ba55fa73ccc87f5e2dc48744658fb020a4
Jeziel Carvalho <jezielcarvalho@ioasys.com.br>
2021-05-10 15:57:34 -0300

    fix: lib context auth add types

--- src/lib/contexts/auth.ts
+++ src/lib/contexts/auth.ts
@@ -1,27 +1,36 @@
+import { UserDataType } from 'interfaces/storage';
+
 const ACCESS_TOKEN = 'access-token';
+const REFRESH_TOKEN = 'refresh-token';
 const NAME = 'name';
 const FULL_NAME = 'fullname';
 const EMAIL = 'email';
 
 export const getAccessToken = () => localStorage.getItem(ACCESS_TOKEN);
 
-export const getUserData = () => {
-  return {
-    name: localStorage.getItem(NAME),
-    email: localStorage.getItem(EMAIL),
-    fullname: localStorage.getItem(FULL_NAME),
-  };
-};
+export const getUserData = (): UserDataType => ({
+  name: localStorage.getItem(NAME),
+  email: localStorage.getItem(EMAIL),
+  fullname: localStorage.getItem(FULL_NAME),
+});
 
 type StorageAuthData = {
-  accessToken: string
-  name: string
-  fullname: string
-  email: string
-}
+  accessToken: string;
+  refreshToken: string;
+  name: string;
+  fullname: string;
+  email: string;
+};
 
-export const setAccessToken = ({ accessToken, name, fullname, email }: StorageAuthData) => {
+export const setAccessToken = ({
+  accessToken,
+  refreshToken,
+  name,
+  fullname,
+  email,
+}: StorageAuthData) => {
   localStorage.setItem(ACCESS_TOKEN, accessToken);
+  localStorage.setItem(REFRESH_TOKEN, refreshToken);
   localStorage.setItem(NAME, name);
   localStorage.setItem(FULL_NAME, fullname);
   localStorage.setItem(EMAIL, email);
