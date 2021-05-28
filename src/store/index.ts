a812248d966ce7ac53471bde05ee8b00dd53225c
Jeziel Carvalho <jezielcarvalho@ioasys.com.br>
2021-05-28 19:21:43 -0300

    add redux logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './index.ducks';
 import Sagas from './index.sagas';
@@ -8,7 +9,9 @@ export type AppDispatch = typeof store.dispatch;
 
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [
-  ...getDefaultMiddleware({ thunk: false, serializableCheck: false }),
+  ...getDefaultMiddleware({ thunk: false, serializableCheck: false }).concat(
+    logger,
+  ),
   sagaMiddleware,
 ];
 
